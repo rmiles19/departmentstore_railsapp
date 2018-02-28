cc318a7a0fddfa4d02e5e5f2e383d6aa2294e0df
Rachel Miles <milesrachel9@gmail.com>
2018-02-28 15:29:40 -0700

    add items and departments controllers

---
+++ app/controllers/departments_controller.rb
@@ -0,0 +1,13 @@
+class DepartmentsController < ApplicationController
+  def index
+    @departments = Department.all
+  end
+
+  def show
+    @department = Department.find(params[:id])
+  end
+
+  def new
+    @department = Department.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'items/index'
+
+  get 'items/show'
+
+  get 'items/new'
+
+  get 'departments/index'
+
+  get 'departments/show'
+
+  get 'departments/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
